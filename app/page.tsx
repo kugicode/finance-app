f69a49ea660898cfc9c97e281f24bb771652119a
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-02-08 22:41:46 +0000

    Fixed layout and updated list structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { Transaction } from "./types/transaction";
 
 export default function Home() {
+  const mockTransactions: Transaction[] = [
+    {id: "1", amount: 5, category: "meal"},
+    {id: "2", amount: 5, category: "Coffee"}
+  ]
   return (
-    <main className="flex h-screen items-center justify-center">
+    <main className="flex flex-col h-screen items-center justify-center">
     <h1 className="text-4xl text-green-600">Finance app</h1>
+    <ul>{mockTransactions.map((m) => 
+    <li key={m.id}>{ m.category }: { m.amount } </li>
+    )}</ul>
     </main>
   );
 }
