7f51acea166f771af43e06ab71af915b2df7dd71
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-02-09 23:36:49 +0000

    Add dynamic state and transaction button

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 import { Transaction } from "./types/transaction";
 
 export default function Home() {
-  const mockTransactions: Transaction[] = [
-    {id: "1", amount: 5, category: "meal"},
-    {id: "2", amount: 5, category: "Coffee"}
-  ]
+  const [transactions, setTransactions] = useState<Transaction[]>( [
+    {id: "1", amount: 5, category: "Meal"},
+    {id: "2", amount: 5, category: "Coffee"} 
+  ]);
+
+  const addItem = () => {
+    setTransactions([...transactions, {id: Date.now().toString() ,amount: 10, category: "Test item"}]);
+  }
+
+  const total = transactions.reduce((acc, current) => acc + current.amount, 0);
+
   return (
     <main className="flex flex-col h-screen items-center justify-center">
     <h1 className="text-4xl text-green-600">Finance app</h1>
-    <ul>{mockTransactions.map((m) => 
+    <button className="bg-green-500 p-2 rounded text-white" onClick={() => addItem()}>add item</button>
+    <ul>{transactions.map((m) => 
     <li key={m.id}>{ m.category }: { m.amount } </li>
     )}</ul>
+    <p>total: { total }</p>
     </main>
   );
 }
