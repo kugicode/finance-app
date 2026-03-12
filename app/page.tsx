79f0598a56137f35d2215aa983e7b529e92c43d0
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-03-12 23:56:54 +0000

    improving the apps responsive design and look using tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -57,14 +57,17 @@ loadData();
   const totalIncome = transactions.filter(t => t.type === 'income').reduce((acc, current) => acc + current.amount, 0);
   const totalExpense = transactions.filter(t => t.type === 'expense').reduce((acc, current) => acc + current.amount, 0);
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
+    <main className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
     <h1 className="text-4xl text-green-600">Finance app</h1>
     <input className="border-2 border-black m-1" type="text" value={category} onChange={(e) => setCategory(e.target.value)}/>
     <input className="border-2 border-black m-1" type="number" value={amount} onChange={(e) => setAmount(Number(e.target.value))}/>
-    <div className="flex flex-row gap-4 mb-6">
-    <p className="text-2xl font-bold bg-white shadow-md p-4 rounded-4xl flex-1">total: { total }</p>
-    <p className="text-2xl font-bold bg-white shadow-md p-4 rounded-4xl flex-1 text-green-600">total income: { totalIncome }</p>
-    <p className="text-2xl font-bold bg-white shadow-md p-4 rounded-4xl flex-1 text-red-600">total expense: { totalExpense }</p>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-4xl mb-8 mx-2">
+    <p className="text-xl font-medium bg-white shadow-sm border border-gray-100 p-6 rounded-2xl text-gray-600">
+      Total Balance: <span className="block text-3xl font-bold text-gray-900 mt-2">{ total }</span></p>
+    <p className="text-xl font-medium bg-white shadow-sm border border-gray-100 p-6 rounded-2xl text-green-600">
+      Total income: <span className="block text-3xl font-bold mt-2">{ totalIncome }</span></p>
+    <p className="text-xl font-medium bg-white shadow-sm border border-gray-100 p-6 rounded-2xl text-red-600">
+      Total expense: <span className="block text-3xl font-bold mt-2">{ totalExpense }</span></p>
     </div>
     <select value={type} onChange={(e) => setType(e.target.value as 'income' | 'expense')}>
       <option value="income">income</option>
