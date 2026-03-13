60d63874e4f0e00ca9702da623c8fac066bebe4f
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-03-13 23:59:12 +0000

    Added more style and making sure it's responsive on both mobile and pc

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
   const [category, setCategory] = useState<string>('');
   const [amount, setAmount] = useState<number>(0);
   const [type, setType] = useState<'income'|'expense'>('expense');
-
 const loadData = async () => {
   //Use await to fetch...
   const res = await fetch('/api/transactions');
@@ -58,10 +57,8 @@ loadData();
   const totalExpense = transactions.filter(t => t.type === 'expense').reduce((acc, current) => acc + current.amount, 0);
   return (
     <main className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
-    <h1 className="text-4xl text-green-600">Finance app</h1>
-    <input className="border-2 border-black m-1" type="text" value={category} onChange={(e) => setCategory(e.target.value)}/>
-    <input className="border-2 border-black m-1" type="number" value={amount} onChange={(e) => setAmount(Number(e.target.value))}/>
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-4xl mb-8 mx-2">
+    <h1 className="text-4xl text-green-600 text-center mb-2">Finance app</h1>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-4xl mb-8 mx-auto">
     <p className="text-xl font-medium bg-white shadow-sm border border-gray-100 p-6 rounded-2xl text-gray-600">
       Total Balance: <span className="block text-3xl font-bold text-gray-900 mt-2">{ total }</span></p>
     <p className="text-xl font-medium bg-white shadow-sm border border-gray-100 p-6 rounded-2xl text-green-600">
@@ -69,11 +66,15 @@ loadData();
     <p className="text-xl font-medium bg-white shadow-sm border border-gray-100 p-6 rounded-2xl text-red-600">
       Total expense: <span className="block text-3xl font-bold mt-2">{ totalExpense }</span></p>
     </div>
-    <select value={type} onChange={(e) => setType(e.target.value as 'income' | 'expense')}>
+    <div className="flex flex-col items-center">
+    <input className="border border-gray-500 m-1 " type="text" placeholder="Enter category..." value={category} onChange={(e) => setCategory(e.target.value)}/>
+    <input className="border border-gray-500 m-1 " type="number" placeholder="Enter price..." value={amount} onChange={(e) => setAmount(Number(e.target.value))}/>
+    <select className="border border-gray-500 m-1 " value={type} onChange={(e) => setType(e.target.value as 'income' | 'expense')}>
       <option value="income">income</option>
       <option value="expense">expense</option>
     </select>
     <button className="bg-green-500 p-2 rounded text-white" onClick={() => addItem()}>add item</button>
+    </div>
     <ul>{transactions.map((m) =>
   <TransactionItem key={m.id} transaction={m} onDelete={deleteItem}/>
     )}</ul>
