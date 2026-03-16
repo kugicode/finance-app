78b53083b80ea01d2fe87837869a721509913fbe
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-03-16 23:59:17 +0000

    making the transactions section look better

--- app/components/TransactionItem.tsx
+++ app/components/TransactionItem.tsx
@@ -8,11 +8,14 @@ interface Props {
 export default function TransactionItem({transaction, onDelete}: Props){
 
     return(
-    <li className="bg-white shadow-md rounded-lg p-4 mb-2 flex justify-between items-center"><div>
-        { transaction.category }:
-        <span className={` ${transaction.type === 'income' ? 'text-green-500' : 'text-red-500'}`}>{ transaction.amount }: </span>
-        {transaction.date}
+    <li className="bg-white shadow-md rounded-lg p-4 mb-2 flex justify-between items-center">
+        <div className="flex flex-col">
+        <p className="font-bold text-gray-900 text-lg leading-tight">{ transaction.category }</p>
+        <p className="text-xs font-semibold text-gray-400 mt-1 uppercase tracking-widest ">{transaction.date}</p>
         </div> 
+        <div>
+            <span className={` ${transaction.type === 'income' ? 'text-green-500' : 'text-red-500'}`}>{ transaction.amount }: </span>
+      </div>
         <button className="bg-red-500 px-3 py-1 rounded" onClick={() => 
         onDelete(transaction.id)}>X</button></li>
     );
