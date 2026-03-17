74b2c05d0b486025296df1ea80322d3602eb68c6
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-03-17 16:46:55 +0000

    Improved the transactions design

--- app/components/TransactionItem.tsx
+++ app/components/TransactionItem.tsx
@@ -1,4 +1,5 @@
 import { Transaction } from "../types/transaction";
+import { Trash2 } from "lucide-react";
 
 interface Props {
     transaction: Transaction,
@@ -9,14 +10,19 @@ export default function TransactionItem({transaction, onDelete}: Props){
 
     return(
     <li className="bg-white shadow-md rounded-lg p-4 mb-2 flex justify-between items-center">
-        <div className="flex flex-col">
+        <div className="ml-4 flex-1">
         <p className="font-bold text-gray-900 text-lg leading-tight">{ transaction.category }</p>
-        <p className="text-xs font-semibold text-gray-400 mt-1 uppercase tracking-widest ">{transaction.date}</p>
+        {/* We use new Date(transaction.date) so it turns the long string back to a Javascript Date object
+        and .toLocalDateString so it formats the date in which country we're in.
+        */}
+        <p className="text-xs font-semibold text-gray-400 mt-1 uppercase tracking-widest ">{new Date(transaction.date).toLocaleDateString()}</p>
         </div> 
-        <div>
-            <span className={` ${transaction.type === 'income' ? 'text-green-500' : 'text-red-500'}`}>{ transaction.amount }: </span>
+        <div className="text-right mr-4">
+            <span className={` ${transaction.type === 'income' ? 'text-green-500' : 'text-red-500'} font-bold text-lg` }>
+               {transaction.type === 'income' ? '+' : '-'}£{ transaction.amount } </span>
       </div>
-        <button className="bg-red-500 px-3 py-1 rounded" onClick={() => 
-        onDelete(transaction.id)}>X</button></li>
+        <button className="bg-red-300 px-3 py-2 rounded cursor-pointer hover:bg-red-500" onClick={() => onDelete(transaction.id)}> 
+            <Trash2 />
+         </button></li>
     );
 }
